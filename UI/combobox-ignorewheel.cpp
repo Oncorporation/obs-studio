349ea16048fefc546d24aeff6c179229e8398e8e
jp9000 <obs.jim@gmail.com>
2019-04-21 04:10:46 -0700

    UI: Rename UI files for consistency
    Changes comboBox-* and spinBox-* files to lowercase to be more
    consistent with the rest of the files in the UI directory.

--- UI/combobox-ignorewheel.cpp
+++ UI/combobox-ignorewheel.cpp
@@ -5,15 +5,10 @@ ComboBoxIgnoreScroll::ComboBoxIgnoreScroll(QWidget *parent) : QComboBox(parent)
 	setFocusPolicy(Qt::StrongFocus);
 }
 
-void ComboBoxIgnoreScroll::wheelEvent(QWheelEvent * event)
+void ComboBoxIgnoreScroll::wheelEvent(QWheelEvent *event)
 {
 	if (!hasFocus())
 		event->ignore();
 	else
 		QComboBox::wheelEvent(event);
-}
-
-void ComboBoxIgnoreScroll::leaveEvent(QEvent * event)
-{
-	clearFocus();
 }
--- UI/spinbox-ignorewheel.cpp
+++ UI/spinbox-ignorewheel.cpp
@@ -5,15 +5,10 @@ SpinBoxIgnoreScroll::SpinBoxIgnoreScroll(QWidget *parent) : QSpinBox(parent)
 	setFocusPolicy(Qt::StrongFocus);
 }
 
-void SpinBoxIgnoreScroll::wheelEvent(QWheelEvent * event)
+void SpinBoxIgnoreScroll::wheelEvent(QWheelEvent *event)
 {
 	if (!hasFocus())
 		event->ignore();
 	else
 		QSpinBox::wheelEvent(event);
-}
-
-void SpinBoxIgnoreScroll::leaveEvent(QEvent * event)
-{
-	clearFocus();
 }
