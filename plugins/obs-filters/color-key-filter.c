a91c208184b5345fa297de8069f87b1cbbff2e86
Charles Fettinger <charles@onacloud.org>
2020-12-29 21:25:37 -0800

    obs-filters:  Chroma Key Format
    Apply Clag formatting

--- plugins/obs-filters/color-key-filter.c
+++ plugins/obs-filters/color-key-filter.c
@@ -26,18 +26,18 @@
 /* clang-format on */
 
 struct color_key_filter_data {
-	obs_source_t *context;
+	obs_source_t* context;
 
-	gs_effect_t *effect;
+	gs_effect_t* effect;
 
-	gs_eparam_t *color_param;
-	gs_eparam_t *contrast_param;
-	gs_eparam_t *brightness_param;
-	gs_eparam_t *gamma_param;
+	gs_eparam_t* color_param;
+	gs_eparam_t* contrast_param;
+	gs_eparam_t* brightness_param;
+	gs_eparam_t* gamma_param;
 
-	gs_eparam_t *key_color_param;
-	gs_eparam_t *similarity_param;
-	gs_eparam_t *smoothness_param;
+	gs_eparam_t* key_color_param;
+	gs_eparam_t* similarity_param;
+	gs_eparam_t* smoothness_param;
 
 	struct vec4 color;
 	float contrast;
@@ -49,14 +49,14 @@ struct color_key_filter_data {
 	float smoothness;
 };
 
-static const char *color_key_name(void *unused)
+static const char* color_key_name(void* unused)
 {
 	UNUSED_PARAMETER(unused);
 	return obs_module_text("ColorKeyFilter");
 }
 
-static inline void color_settings_update(struct color_key_filter_data *filter,
-					 obs_data_t *settings)
+static inline void color_settings_update(struct color_key_filter_data* filter,
+	obs_data_t* settings)
 {
 	uint32_t opacity =
 		(uint32_t)obs_data_get_int(settings, SETTING_OPACITY);
@@ -66,7 +66,7 @@ static inline void color_settings_update(struct color_key_filter_data *filter,
 	double gamma = obs_data_get_double(settings, SETTING_GAMMA);
 
 	contrast = (contrast < 0.0) ? (1.0 / (-contrast + 1.0))
-				    : (contrast + 1.0);
+		: (contrast + 1.0);
 
 	brightness *= 0.5;
 
@@ -79,14 +79,14 @@ static inline void color_settings_update(struct color_key_filter_data *filter,
 	vec4_from_rgba(&filter->color, color);
 }
 
-static inline void key_settings_update(struct color_key_filter_data *filter,
-				       obs_data_t *settings)
+static inline void key_settings_update(struct color_key_filter_data* filter,
+	obs_data_t* settings)
 {
 	int64_t similarity = obs_data_get_int(settings, SETTING_SIMILARITY);
 	int64_t smoothness = obs_data_get_int(settings, SETTING_SMOOTHNESS);
 	uint32_t key_color =
 		(uint32_t)obs_data_get_int(settings, SETTING_KEY_COLOR);
-	const char *key_type =
+	const char* key_type =
 		obs_data_get_string(settings, SETTING_COLOR_TYPE);
 
 	if (strcmp(key_type, "green") == 0)
@@ -104,17 +104,17 @@ static inline void key_settings_update(struct color_key_filter_data *filter,
 	filter->smoothness = (float)smoothness / 1000.0f;
 }
 
-static void color_key_update(void *data, obs_data_t *settings)
+static void color_key_update(void* data, obs_data_t* settings)
 {
-	struct color_key_filter_data *filter = data;
+	struct color_key_filter_data* filter = data;
 
 	color_settings_update(filter, settings);
 	key_settings_update(filter, settings);
 }
 
-static void color_key_destroy(void *data)
+static void color_key_destroy(void* data)
 {
-	struct color_key_filter_data *filter = data;
+	struct color_key_filter_data* filter = data;
 
 	if (filter->effect) {
 		obs_enter_graphics();
@@ -125,11 +125,11 @@ static void color_key_destroy(void *data)
 	bfree(data);
 }
 
-static void *color_key_create(obs_data_t *settings, obs_source_t *context)
+static void* color_key_create(obs_data_t* settings, obs_source_t* context)
 {
-	struct color_key_filter_data *filter =
+	struct color_key_filter_data* filter =
 		bzalloc(sizeof(struct color_key_filter_data));
-	char *effect_path = obs_module_file("color_key_filter.effect");
+	char* effect_path = obs_module_file("color_key_filter.effect");
 
 	filter->context = context;
 
@@ -166,12 +166,12 @@ static void *color_key_create(obs_data_t *settings, obs_source_t *context)
 	return filter;
 }
 
-static void color_key_render(void *data, gs_effect_t *effect)
+static void color_key_render(void* data, gs_effect_t* effect)
 {
-	struct color_key_filter_data *filter = data;
+	struct color_key_filter_data* filter = data;
 
 	if (!obs_source_process_filter_begin(filter->context, GS_RGBA,
-					     OBS_ALLOW_DIRECT_RENDERING))
+		OBS_ALLOW_DIRECT_RENDERING))
 		return;
 
 	gs_effect_set_vec4(filter->color_param, &filter->color);
@@ -187,56 +187,56 @@ static void color_key_render(void *data, gs_effect_t *effect)
 	UNUSED_PARAMETER(effect);
 }
 
-static bool key_type_changed(obs_properties_t *props, obs_property_t *p,
-			     obs_data_t *settings)
+static bool key_type_changed(obs_properties_t* props, obs_property_t* p,
+	obs_data_t* settings)
 {
-	const char *type = obs_data_get_string(settings, SETTING_COLOR_TYPE);
+	const char* type = obs_data_get_string(settings, SETTING_COLOR_TYPE);
 	bool custom = strcmp(type, "custom") == 0;
 
 	obs_property_set_visible(obs_properties_get(props, SETTING_KEY_COLOR),
-				 custom);
+		custom);
 
 	UNUSED_PARAMETER(p);
 	return true;
 }
 
-static obs_properties_t *color_key_properties(void *data)
+static obs_properties_t* color_key_properties(void* data)
 {
-	obs_properties_t *props = obs_properties_create();
+	obs_properties_t* props = obs_properties_create();
 
-	obs_property_t *p = obs_properties_add_list(props, SETTING_COLOR_TYPE,
-						    TEXT_COLOR_TYPE,
-						    OBS_COMBO_TYPE_LIST,
-						    OBS_COMBO_FORMAT_STRING);
+	obs_property_t* p = obs_properties_add_list(props, SETTING_COLOR_TYPE,
+		TEXT_COLOR_TYPE,
+		OBS_COMBO_TYPE_LIST,
+		OBS_COMBO_FORMAT_STRING);
 	obs_property_list_add_string(p, obs_module_text("Green"), "green");
 	obs_property_list_add_string(p, obs_module_text("Blue"), "blue");
 	obs_property_list_add_string(p, obs_module_text("Red"), "red");
 	obs_property_list_add_string(p, obs_module_text("Magenta"), "magenta");
 	obs_property_list_add_string(p, obs_module_text("CustomColor"),
-				     "custom");
+		"custom");
 
 	obs_property_set_modified_callback(p, key_type_changed);
 
 	obs_properties_add_color(props, SETTING_KEY_COLOR, TEXT_KEY_COLOR);
 	obs_properties_add_int_slider(props, SETTING_SIMILARITY,
-				      TEXT_SIMILARITY, 1, 1000, 1);
+		TEXT_SIMILARITY, 1, 1000, 1);
 	obs_properties_add_int_slider(props, SETTING_SMOOTHNESS,
-				      TEXT_SMOOTHNESS, 1, 1000, 1);
+		TEXT_SMOOTHNESS, 1, 1000, 1);
 
 	obs_properties_add_int_slider(props, SETTING_OPACITY, TEXT_OPACITY, 0,
-				      100, 1);
+		100, 1);
 	obs_properties_add_float_slider(props, SETTING_CONTRAST, TEXT_CONTRAST,
-					-1.0, 1.0, 0.01);
+		-1.0, 1.0, 0.01);
 	obs_properties_add_float_slider(props, SETTING_BRIGHTNESS,
-					TEXT_BRIGHTNESS, -1.0, 1.0, 0.01);
+		TEXT_BRIGHTNESS, -1.0, 1.0, 0.01);
 	obs_properties_add_float_slider(props, SETTING_GAMMA, TEXT_GAMMA, -1.0,
-					1.0, 0.01);
+		1.0, 0.01);
 
 	UNUSED_PARAMETER(data);
 	return props;
 }
 
-static void color_key_defaults(obs_data_t *settings)
+static void color_key_defaults(obs_data_t* settings)
 {
 	obs_data_set_default_int(settings, SETTING_OPACITY, 100);
 	obs_data_set_default_double(settings, SETTING_CONTRAST, 0.0);
